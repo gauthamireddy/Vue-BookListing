fe30ca9ed9eb8b51170efd4e88f186b6ee9427e0
gauthami <ggadda188@comcast.com>
2018-10-15 13:39:51 -0400

    Style Updates for listed item

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
     <div>   
     <h1> {{ title }} </h1>
     <ul>
-        <li v-for = "book in books"> {{ book.title }}; {{book.author}} </li>
+        <li v-for = "book in books"> {{ book.title }} : {{book.author}} </li>
     </ul>
     </div>
 </template>
@@ -34,7 +34,7 @@ ul {
     padding: 0;
 }
 
-ul > li {
+li {
     display: block;
     margin: 0 10px;
 }
